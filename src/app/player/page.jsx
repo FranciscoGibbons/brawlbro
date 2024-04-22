c69a4feca3d337e09fc1c309bd8b3a053c1d0b06
Francisco Gibbons <franchugibbdev@gmail.com>
2024-04-22 19:25:18 -0300

    testing 1

--- src/app/player/page.jsx
+++ src/app/player/page.jsx
@@ -6,6 +6,7 @@ export default function PlayersPage() {
     <div className="flex min-h-screen flex-col items-center gap-y-40 p-24">
       <h1 className="text-5xl font-bold">Write your ID</h1>
       <InputPlayer />
+      a
       <Image width={500} height={400} src={'/id.webp'} alt="" />
     </div>
   );
